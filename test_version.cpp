17c94c619d9afd7d95de11e788359af82745d52c
ivanleb <ivan_leb@mail.ru>
2018-10-29 18:39:12 +0300

    add filter test

--- test_version.cpp
+++ test_version.cpp
@@ -31,7 +31,28 @@ BOOST_AUTO_TEST_CASE(ip_filter_test_filter_any)
             BOOST_CHECK( compResult );
         }
     }
-    //BOOST_CHECK();
+}
+
+BOOST_AUTO_TEST_CASE(ip_filter_test_filter)
+{
+    ip_container container;
+    std::vector<std::string> ip1 = {"1", "2", "3", "4"};
+    std::vector<std::string> ip2 = {"2", "5", "3", "4"};
+    std::vector<std::string> ip3 = {"2", "10", "3", "4"};
+    container.push_back(ip1);
+    container.push_back(ip2);
+    container.push_back(ip3);
+
+    ip_container result = filter(container, 1);
+
+    for(const auto& vs: result)
+    {
+        for(size_t i = 0; i < vs.size(); ++i)
+        {
+            bool compResult = !((bool)(vs[i].compare(ip1[i])));
+            BOOST_CHECK( compResult );
+        }
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
